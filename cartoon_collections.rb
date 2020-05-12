c69164b9b4520212f8264189542bfee4921a3e99
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-12 18:13:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,8 +22,12 @@ def summon_captain_planet(array)
 end
 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  i = 0 
+  
+  while i < array.length
+    
+    i += 1
 end
 
 def find_the_cheese# code an argument here
