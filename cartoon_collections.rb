ba01996f16b2cfd6a125a8a941a95da921c9c304
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-12 18:02:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,7 @@ def summon_captain_planet(array)
   
 end
 
+=begin
 def long_planeteer_calls# code an argument here
   # Your code here
 end
@@ -31,3 +32,5 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+=end
+
